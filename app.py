cbf0034da2b63f397b1db1dd4ecec3197ab1fbca
prtm1908 <prathambatra19@gmail.com>
2023-11-02 11:45:15 +0530

    files

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from main import get_qa_chain, create_vector_db
+
+st.title("Q&A 🏫")
+btn = st.button("Create Knowledgebase")
+if btn:
+    create_vector_db()
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_qa_chain()
+    response = chain(question)
+
+    st.header("Answer")
+    st.write(response["result"])
